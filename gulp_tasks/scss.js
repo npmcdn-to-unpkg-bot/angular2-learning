849a1d7e7ddb90a7fa310d0439571b443fdb4d0f
Chen Zhiguang <zhiguang.chen@hanlogy.com>
2016-08-11 16:24:15 +0800

    create scss gulp task and styles.css

---
+++ gulp_tasks/scss.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp         = require( "gulp" );
+var sass         = require( 'gulp-sass' );
+var sourcemaps   = require( 'gulp-sourcemaps' );
+var errorHandler = require( './error-handler.js' );
+
+
+gulp.task( 'scss', function () {
+  return scssToCss( "./src/styles.scss", "./" );
+} );
+
+
+
+function scssToCss ( srcFiles, destPath, srcOptions ) {
+  return gulp.src( srcFiles, srcOptions||null )
+    .pipe( sourcemaps.init() )
+    .pipe( errorHandler.start() )
+    .pipe( sass().on( 'error', sass.logError ) )
+    .pipe( errorHandler.stop() )
+    .pipe(sourcemaps.write())
+    .pipe( gulp.dest( destPath ) );
+}
