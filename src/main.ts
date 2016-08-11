9ac8c0866a3e902e9afffa1c11ad1bede813c25f
Chen Zhiguang <zhiguang.chen@hanlogy.com>
2016-08-11 16:12:16 +0800

    create main typescript files

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+///<reference path="../typings/index.d.ts"/>
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
